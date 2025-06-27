0cb07de7297ab98f76761f4e93c4a53cafc4082f
Dylan Tong <dylantong17@gmail.com>
2025-06-27 12:21:34 +1200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import coinHead from "./assets/head.png";
+import coinTail from "./assets/tail.png";
+import "./App.css";
+
+function App() {
+  const [coin, setCoin] = useState(coinHead);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="main-title">Filp the coin</h1>
+
+        <div className="coin-container">
+          <img src={coinHead} alt="coin" className="coin" />
+        </div>
+        <div className="button-container">
+          <button>Filp the coin</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
