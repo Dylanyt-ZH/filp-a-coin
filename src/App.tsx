eea3c69a246fbda26a235c9ec35b5bcb3d377b79
Dylan Tong <dylantong17@gmail.com>
2025-06-28 14:39:34 +1200

    add flip function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import coinHead from "./assets/head.png";
-// import coinTail from "./assets/tail.png";
+import coinTail from "./assets/tail.png";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [coinFace, setCoinFace] = useState("head");
+
   return (
     <>
       <div className="container">
         <h1 className="main-title">Flip the coin</h1>
 
         <div className="coin-container">
-          <img src={coinHead} alt="coin" className="coin" />
+          <img
+            src={coinFace === "head" ? coinHead : coinTail}
+            alt="coin"
+            className="coin"
+          />
         </div>
         <div className="button-container">
-          <button>Flip the coin</button>
+          <button
+            onClick={() =>
+              coinFace === "head" ? setCoinFace("tail") : setCoinFace("head")
+            }
+          >
+            Flip the coin
+          </button>
         </div>
       </div>
     </>
